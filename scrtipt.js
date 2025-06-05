dcd14fa823a92120a1d15890fc0fa5df67d15357
Treyser044 <treyser044@gmail.com>
2025-06-05 12:13:40 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,24 @@
+console.log('Gay');
+
+
+var student1 = 'GUY'
+let student2 = 'GEY'
+const student3 = 'GIY'
+
+/*console.log(student1);
+console.log(student2);
+console.log(student3);*/
+
+alert('ya ne gay 🧄')
+confirm('OSHIBAYESHSYA YA NE GAY 🤧')
+
+let name = prompt('YA NE GEEEEEY SKASHI SVOYO IMYA YA NAYDU TEBYA')
+let like = prompt('like')
+let dontlike = prompt('dontlike')
+ 
+
+console.log(name);
+console.log(like);
+console.log(dontlike);
+
+alert('Ozini lox deb oylaydigan isqirt ' + name + '\n unga sex qilishga yaraydigan odam' + like + 'Unga sex qilishga yaramaydigan odam ' + dontlike )
